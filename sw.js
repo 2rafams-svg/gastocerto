db6b81f922118d6c03d80ff056a8d65f470a6348
2rafams-svg <2rafams@gmail.com>
2026-06-11 14:20:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'gastocerto-v1.5';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Supabase e qualquer cross-origin: sempre rede (dados frescos)
+  if (url.origin !== location.origin) return;
+  // Shell: cache primeiro, atualiza em background (stale-while-revalidate)
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});

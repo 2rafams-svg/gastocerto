17d258e2573a82f5ee85383052457c09e344c16c
2rafams-svg <2rafams@gmail.com>
2026-06-23 15:22:44 -0300

    Sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'gastocerto-v3.10-activity-icon';
+﻿const CACHE = 'gastocerto-v3.11-responsive';
 const SHELL = ['./', './index.html', './offline.html', './style.css', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

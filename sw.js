eada6a22e266a06e7d4145dfb2db8e900b664289
2rafams-svg <2rafams@gmail.com>
2026-06-25 23:57:25 -0300

    Sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'gastocerto-v3.30-responsive';
-const SHELL = ['./', './index.html', './offline.html', './style.css?v=3.30', './app.js?v=3.30', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'gastocerto-v3.31-top-nav';
+const SHELL = ['./', './index.html', './offline.html', './style.css?v=3.31', './app.js?v=3.31', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
